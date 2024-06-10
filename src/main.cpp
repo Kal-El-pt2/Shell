edfa83ac94713527b9d818a5cb4b2afe42e2672b
Kal-El-pt2 <f20220052@hyderabad.bits-pilani.ac.in>
2024-06-10 05:43:07 +0530

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  while(true){
   std::cout<<"$ ";
   std::string input;
   std::getline(std::cin,input);
   std::cout << input << ": command not found\n";
+  }
 }
